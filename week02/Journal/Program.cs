abb6516e34eeab9b3a1ae589b4dafbd1b22d1515
Igor2games <igor1games@outlook.com>
2025-09-10 21:41:52 -0300

    started journal assigment

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        string menu = "";
+        //Add Journal
+        Journal theJournal = new Journal();
+
+        while (menu != "5")
+        {
+            Console.WriteLine("Please select one of the following choices:\n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+            Console.Write("What would you like to do? ");
+            menu = Console.ReadLine();
+
+            if (menu == "1")
+            {
+                //Add entry and prompts
+                Entry anEntry = new Entry();
+                PromptGenerator randomPrompt = new PromptGenerator();
+                string prompt = randomPrompt.GetRandomPrompt();
+
+                //Entry date
+                DateTime theCurrentTime = DateTime.Now;
+                string datetext = theCurrentTime.ToShortDateString();
+                anEntry._date = datetext;
+
+                //Generate random prompt
+                Console.WriteLine(prompt);
+                anEntry._promptText = prompt;
+                Console.Write(">");
+                string text = Console.ReadLine();
+                anEntry._entryText = text;
+
+                //Add to Journal
+                theJournal.AddEntry(anEntry);
+
+            }
+
+            if (menu == "2")
+            {
+                theJournal.DisplayAll();
+            }
+        }
     }
 }
