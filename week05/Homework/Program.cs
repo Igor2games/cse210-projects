8d073d3549689fffbc5fec65ec7d8ff817a12a9d
Igor2games <igor1games@outlook.com>
2025-09-29 18:46:14 -0300

    homerok math

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -6,5 +6,10 @@ class Program
     {
         Assignment assignment1 = new Assignment("Samuel Bennet", "Multiplication");
         Console.WriteLine($"{assignment1.GetSummary()}");
+        Console.WriteLine("");
+
+        MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
     }
 }
