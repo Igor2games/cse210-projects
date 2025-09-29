69fe5c2de1d77e4d906a1e78538e6dfa4610a6da
Igor2games <igor1games@outlook.com>
2025-09-29 18:52:36 -0300

    homework finished

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -11,5 +11,10 @@ class Program
         MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(mathAssignment1.GetSummary());
         Console.WriteLine(mathAssignment1.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment wassigment1 = new WritingAssignment("Mary Waters", "Europen History", "The Causes of World War II");
+        Console.WriteLine(wassigment1.GetSummary());
+        Console.WriteLine(wassigment1.GetWritingInformation());
     }
 }
