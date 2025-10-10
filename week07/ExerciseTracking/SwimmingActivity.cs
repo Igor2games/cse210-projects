d31bd5cd427b9730520d915cafcb7cd07e27a242
Igor2games <igor1games@outlook.com>
2025-10-10 18:50:28 -0300

    saving

--- week07/ExerciseTracking/SwimmingActivity.cs
+++ week07/ExerciseTracking/SwimmingActivity.cs
@@ -1,4 +1,32 @@
-public class SwimmingActivity
+public class SwimmingActivity : Activity
 {
-    
+    private int _laps;
+    private string _name = "Swimming";
+
+    public SwimmingActivity(string date, int lenght, int laps) : base(date, lenght)
+    {
+        _date = date;
+        _lenght = lenght;
+        _laps = laps;
+    }
+
+    public override string GetName()
+    {
+        return _name;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50 / 1000;
+    }
+
+    public override double GetSpeed()
+    {
+        return GetDistance() / _lenght * 60;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
 }
